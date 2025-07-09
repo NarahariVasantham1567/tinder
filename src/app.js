0236b14ebcd74409ba5ff90ced74f931ce738b58
NarahariVasantham1567 <naraharivasantham1567@gmail.com>
2025-07-09 19:53:35 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -6,18 +6,6 @@ app.get('/user', (req, res) => {
   res.send({ firstName: 'Akshay', lastName: 'Saini' });
 });
 
-app.post('/user', (req, res) => {
-  res.send('Data successfully send to the database!');
-});
-
-app.delete('/user', (req, res) => {
-  res.send('Deleted successfully');
-});
-
-app.get('/test', (req, res) => {
-  res.send('Hello from the server!');
-});
-
 app.listen(3005, () => {
   console.log('Server is successfully running on port: 3005');
 });
