e0e56be33ece2ca5a13b834a60cf22cac84d1312
NarahariVasantham1567 <naraharivasantham1567@gmail.com>
2025-07-09 19:32:13 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.get('/user', (req, res) => {
+  res.send({ firstName: 'Akshay', lastName: 'Saini' });
+});
+
+app.post('/user', (req, res) => {
+  res.send('Data successfully send to the database!');
+});
+
+app.delete('/user', (req, res) => {
+  res.send('Deleted successfully');
+});
+
 app.get('/test', (req, res) => {
   res.send('Hello from the server!');
 });
